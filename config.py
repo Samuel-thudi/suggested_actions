972f447cc99266f3e79b178aaf082d3329c423fc
Samuel <41022861+Samuel-thudi@users.noreply.github.com>
2023-12-04 14:45:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "69033c7d-aa4d-47bc-91c6-6f194f2c100d")
-    # APP_ID = os.environ.get("MicrosoftAppId", "360bcbc8-339f-49d9-8da4-be9f8e345292")
+    #APP_ID = os.environ.get("MicrosoftAppId", "69033c7d-aa4d-47bc-91c6-6f194f2c100d")
+    APP_ID = os.environ.get("MicrosoftAppId", "360bcbc8-339f-49d9-8da4-be9f8e345292")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "TSM8Q~_zkHcNtaRwicuYKo3lWqFulXyonC2GRbty")
