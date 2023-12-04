d8996f3fafae5dde7e882064591895bd01a6c674
Samuel Thudi <samuel.thudi@gmail.com>
2023-12-04 12:51:32 +0530

    suggestd actions

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "360bcbc8-339f-49d9-8da4-be9f8e345292")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "TSM8Q~_zkHcNtaRwicuYKo3lWqFulXyonC2GRbty")
